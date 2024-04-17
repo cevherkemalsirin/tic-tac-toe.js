bc17e78a5b2e2f9c9533f22caa3819f5d1edc8ac
cevher kemal sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2024-04-17 20:03:32 +0300

    Stylize Start and Board

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector(".StartGame");
+const container = document.querySelector(".Container");
+
+startBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    container.classList.toggle("hidden");
+});
+
+const Gameboard = (function(){ 
+    const square = Object.freeze({
+        X : "X",
+        O : "O",
+        empty : " "
+    });
+    return {
+        board : new Array(3).fill().map(()=> new Array(3).fill(square.X)),
+        square
+    };
+})();
+ 
+
+function Person(name, marker)
+{
+    return {name,marker};
+}
 
